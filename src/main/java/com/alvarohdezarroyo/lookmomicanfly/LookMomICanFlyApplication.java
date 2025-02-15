13dabb71d5187b2ccd032ab32a1f2b321776130e
Arroyo <wdonarroyo@gmail.com>
2025-02-15 19:24:16 +0100

    Initial commit

---
+++ src/main/java/com/alvarohdezarroyo/lookmomicanfly/LookMomICanFlyApplication.java
@@ -0,0 +1,13 @@
+package com.alvarohdezarroyo.lookmomicanfly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LookMomICanFlyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LookMomICanFlyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alvarohdezarroyo/lookmomicanfly/LookMomICanFlyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alvarohdezarroyo.lookmomicanfly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LookMomICanFlyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
