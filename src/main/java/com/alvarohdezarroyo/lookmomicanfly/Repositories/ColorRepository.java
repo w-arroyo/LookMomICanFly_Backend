cb3ae338edf08eea51ac4986b966d4e17c0cd682
Arroyo <wdonarroyo@gmail.com>
2025-03-21 15:15:36 +0100

    TRACKING NUMBERS IMPLEMENTED

--- src/main/java/com/alvarohdezarroyo/lookmomicanfly/Repositories/ColorRepository.java
+++ src/main/java/com/alvarohdezarroyo/lookmomicanfly/Repositories/ColorRepository.java
@@ -1,7 +1,9 @@
 package com.alvarohdezarroyo.lookmomicanfly.Repositories;
 
 import com.alvarohdezarroyo.lookmomicanfly.Models.Color;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
@@ -11,7 +13,9 @@ public interface ColorRepository extends JpaRepository<Color,String> {
 
     Optional<Color> findByName(String name);
 
-    @Query(value = "INSERT INTO colors_products VALUES (':productId',':colorId')", nativeQuery = true)
+    @Modifying
+    @Transactional
+    @Query(value = "INSERT INTO colors_products VALUES (:productId,:colorId)", nativeQuery = true)
     void insertProductColors(@Param("productId") String productId, @Param("colorId") String colorId);
 
 }
